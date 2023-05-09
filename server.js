df5985eceb0747f5408ed897ba66eadc696841d7
HueVu <huevt.jp@gmail.com>
2023-05-09 13:31:16 +0700

    first commit

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const Web3 = require('web3');
 const web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545/');
 
 
-const walletAddress = '0xa43d14A80881FaBDE72D97d82E0845d8CC5f09FE';
-const privateKey = '45f7e9a7bfeda67843fcf90a20dc38ada4488f11e39057523bd4ced6e2132f10';
+const walletAddress = 'xxxxx';
+const privateKey = 'xxxxx';
 
-const ETHTokenAddress = '0x8BaBbB98678facC7342735486C851ABD7A0d17Ca';
-const USDTTokenAddress = '0x7ef95a0FEE0Dd31b22626fA2e10Ee6A223F8a684';
-const BUSDTokenAddress = '0xaB1a4d4f1D656d2450692D237fdD6C7f9146e814';
+const ETHTokenAddress = 'xxxxx';
+const USDTTokenAddress = 'xxxxx';
+const BUSDTokenAddress = 'xxxxx';
 
-const recipientWalletAddress = '0x48fB1fa9B7b3855bF1A221A0B2Fad8Ee25aab361';
+const recipientWalletAddress = 'xxxxx';
 
 // create new wallet
 const { createNewWallet } = require('./controllers/create_new_wallet');
